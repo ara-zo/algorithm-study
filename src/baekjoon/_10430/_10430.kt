55cbfd03089f6556127e2fb3a2b61c1a0d2c7816
ara-zo <jo91kr@gmail.com>
2025-08-26 23:16:45 +0900

    10430

---
+++ src/baekjoon/_10430/_10430.kt
@@ -0,0 +1,9 @@
+package _10430
+
+fun main() {
+    val (A, B, C) = readln().split(" ").map { it.toInt() }
+    println((A + B) % C)
+    println(((A % C) + (B % C)) % C)
+    println((A * B) % C)
+    println(((A % C) * (B % C)) % C)
+}
