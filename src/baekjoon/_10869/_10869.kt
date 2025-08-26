3479c2b421577517e48e6c8f7e940c6b76731b22
ara-zo <jo91kr@gmail.com>
2025-08-27 00:10:12 +0900

    10869

---
+++ src/baekjoon/_10869/_10869.kt
@@ -0,0 +1,10 @@
+package _10869
+
+fun main() {
+    val (A, B) = readln().split(" ").map { it.toInt() }
+    println(A + B)
+    println(A - B)
+    println(A * B)
+    println(A / B)
+    println(A % B)
+}
